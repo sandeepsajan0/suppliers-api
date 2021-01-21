033bcaf17ad17e3d99094e210ff9f1f59fd5bd19
sandeepsajan0 <sandeepsajan0@gmail.com>
2021-01-21 15:45:29 +0530

    remove static dir

--- simfoni_task/settings.py
+++ simfoni_task/settings.py
@@ -161,7 +161,6 @@ SIMPLE_JWT = {
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 
 LOGGING = {
     "version": 1,
